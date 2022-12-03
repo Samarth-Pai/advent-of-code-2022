0b15089221ad222723a9eb9541a8bbd8b54757e9
Samarth Pai <samarthpai9870@gmail.com>
2022-12-03 20:35:15 +0530

    Commit at 8:35 pm ist

--- day03/part1.py
+++ day03/part1.py
@@ -1,15 +1,15 @@
 import string
 with open("input.txt") as f:
     lines = f.readlines()
-    alphabetPriorities = set()
+    alphabetPriorities = []
     for line in lines:
         halfStringLength = int(len(line) / 2)
         firstHalfLine = line[:halfStringLength]
         secondHalfLine = line[halfStringLength:]
         for alphabet in firstHalfLine:
             if alphabet in secondHalfLine:
-                if alphabet not in alphabetPriorities:
-                    alphabetPriorities.add(alphabet)
+                alphabetPriorities.append(alphabet)
+                break
     combinations = dict(zip(string.ascii_letters,range(1,53)))
     priorityNumbers = list(map(lambda x:combinations[x],alphabetPriorities))
     print(sum(priorityNumbers))
